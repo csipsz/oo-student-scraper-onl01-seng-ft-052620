07d5ddbfc4026982f9137262b18cfcc949aa0247
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-13 22:02:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,29 @@
-class Student
+
+class Student 
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |kid_hash| 
+      Student.new(kid_hash)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
-    
-  end
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)} 
+    self 
+    end   
+  
 
   def self.all
-    
+    @@all
   end
 end
 
