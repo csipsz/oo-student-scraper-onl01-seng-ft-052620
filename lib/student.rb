0f736135717914d9f00b20d33836a2d05822dc6d
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-13 22:02:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send(("#{key}="), value)} 
     self 
-    end   
+  end   
   
 
   def self.all
